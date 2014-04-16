20daf5c0f235ac4b6e2c11c4b8340bcc116852b0
James Palawaga <j.palawaga@uleth.ca>
2014-04-15 19:40:50 -0600

    Non-nested loops working

--- mainproject.scm
+++ mainproject.scm
@@ -1,8 +1,8 @@
 (define first car)
 (define rest cdr)
 (define stack '())
-(define stackTemp '())
 (define tempArea '())
+(define currentCommand '())
 
 (define (read-keyboard-as-string) ; this function returns keyboard input as a string
             (let ((char (read-char)))
@@ -34,6 +34,12 @@
   )
 
 (define (POP)
+  (define x (first stack))
+  (set! stack (rest stack))
+  x
+  )
+
+(define (SAVE_POP)
   (set! tempArea (first stack))
   (set! stack (rest stack))
   tempArea
@@ -49,8 +55,8 @@
 
 (define (SWAP)
   (define x (first stack))
-  (pop)
-  (pop)
+  (SAVE_POP)
+  (SAVE_POP)
   (push_back x)
   (SAVE)
   )
@@ -66,10 +72,30 @@
      (STACK (rest x)))
   ))
 
+
+; truth condition is already on top of the stack
+(define (LOOP x)
+  ; Assuming that "LOOP" is parsed
+  ; if true (?)
+  ;   If pool
+  ;      parse-input currentCommand
+  ;   If not
+  ;      execute first x
+  (cond
+    ((string-ci=? "pool" (first x)) (parse-input currentCommand))
+    (else 
+     (begin
+      (parse-input (list (first x)))
+      (LOOP (rest x)))
+     )
+   )
+  )
+
 (define (main)
   (display "UofL> ")
   (let
    ((input-list (regexp-split #px" " (read-keyboard-as-string))))
+    (set! currentCommand input-list)
     (parse-input input-list)
     (main)
   )
@@ -85,14 +111,15 @@
     ((string=? ">" (first input)) (push_back (> (POP) (POP))))
     ((string=? "<" (first input)) (push_back (< (POP) (POP))))
     ((string=? ">=" (first input)) (push_back (>= (POP) (POP))))
-    ((string=? "<=" (first input)) (push_back (<= (POP) (POP))))
+    ((string=? "<=" (first input)) (begin (SWAP) (push_back (<= (POP) (POP)))))
     ((string-ci=? "swap" (first input)) (SWAP))
     ((string-ci=? "dup" (first input)) (DUP))
-    ((string-ci=? "pop" (first input)) (POP))
+    ((string-ci=? "pop" (first input)) (SAVE_POP))
     ((string-ci=? "save" (first input)) (SAVE))
     ((string-ci=? "drop" (first input)) (DROP))
     ((string-ci=? "clear" (first input)) (CLEAR))
     ((string-ci=? "stack" (first input)) (STACK stack))
+    ((string-ci=? "loop" (first input)) (if (first stack) (LOOP (rest input) ) () ))
     ((string-ci=? "." (first input)) 
      ( let ()
       (if (> (length input) 1)
@@ -100,7 +127,7 @@
           (display (first stack)))
       (display #\newline)))
     )
-   (when (> (length (rest input)) 0)
+   (when (and (> (length (rest input)) 0) (not (string-ci=? "loop" (first input))))
        (parse-input (rest input))
        
      )
@@ -109,3 +136,5 @@
 (define (string->stringList input)
   (apply string-append ( rest ( apply append (map (lambda (x) (list " " x)) (rest input)))))
 )
+
+(main)
