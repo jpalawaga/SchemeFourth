91a16ac024f3c385f113ad798c9e5c648bd3ef13
Andrew Dickson <andrew.dickson@uleth.ca>
2014-04-16 21:53:23 -0600

    User defined functions: checks for existing function name when calling

--- mainproject.scm
+++ mainproject.scm
@@ -4,8 +4,9 @@
 (define tempArea '())
 (define currentCommand '())
 (define curFunc '())
-(define funcList (make-hash-table) )
+(define funcList (make-hash-table 'equal ) )
 (define funcDefStarted #f)
+(define funcNames '() )
 
 (define (read-keyboard-as-string) ; this function returns keyboard input as a string
             (let ((char (read-char)))
@@ -100,10 +101,12 @@
       ;create a new hash with the function name as the first element of commands list with the result of the commands until "CNUF" 
       (begin ; definition not started yet
        (set! funcDefStarted #t) ; start definition
+       (set! funcNames (append funcNames (list (first funcCommands)) ) )
        (hash-table-put! funcList (first funcCommands) (FUNC (rest funcCommands) ) ) 
        (set! funcDefStarted #f)
        ; display resulting function 
-       (display (hash-table-get funcList (first funcCommands) ) )
+       ;(display (hash-table-get funcList (first funcCommands) ) )
+       (display funcNames)
        (display #\newline)
        
         
@@ -114,14 +117,19 @@
           ; it is a string
           (if (string-ci=? (first funcCommands) "CNUF" ) ;is the string "CNUF" 
               ; it is "CNUF", return empty list
-              (begin 
-                (parse-input (rest funcCommands))
-                 '()
-                 )
+              (if (not (= (length (rest funcCommands)) 0) ) ;is the rest of the command list empty 
+                  (begin ; it is not empty, do commands and return an empty list
+                    (parse-input (rest funcCommands))
+                    '()
+                    )
+                   ; it is empty, just return empty list
+                   '()
+                  )
+              
               ; it isn't "CNUF", return result of rest of command list by appending first element and the result of calling FUNC with the rest of the list
               (append (list (first funcCommands) ) (FUNC (rest funcCommands) ) )
-                       ; should return the resulting list after both appends  
-               )
+              ; should return the resulting list after both appends  
+              )
           ; it isn't a string 
            (append (list (first funcCommands)) (FUNC (rest funcCommands) ) )
           
@@ -129,7 +137,26 @@
       ); end of function definition if
   
   )
+
+(define (callUserFunc funcCall) 
+  (parse-input (hash-table-get funcList funcCall) )
   
+  )
+
+(define (checkExistingFuncs name list) 
+  (define exists #f)
+  (if (> (length list) 0) ;if we haven't hit the end of the list
+      (if (string=? name (first list) ) ;check to see if the names are equal 
+          (set! exists #t) ;they are equal, set exists to true 
+          (set! exists (checkExistingFuncs name (rest list) )) ;they still are not equal, make a recursive call with he rest of the list and set exists to the result  
+           )
+      exists ;we hit the end of the list, base case, return exists 
+      )
+  exists ;return the result 
+  )
+  
+                          
+   
   
 
 (define (main)
@@ -144,6 +171,8 @@
 
 (define parse-input (lambda (input)
   (cond
+    ((string-ci=? "" (first input)) )
+    ((string-ci=? "\t" (first input)) )
     ((string->number (first input)) (push_back (string->number (first input))))
     ((string=? "+" (first input)) (push_back (+ (POP) (POP))))
     ((string=? "-" (first input)) (push_back (- (POP) (POP))))
@@ -166,9 +195,18 @@
      ( let ()
         (if (> (length input) 1)
             (display (string->stringList input))
-            (display (first stack)))
+            (display (first stack))) 
         (display #\newline)))
+    (else (if (checkExistingFuncs (first input) funcNames) 
+              (callUserFunc (first input) ) 
+              (begin 
+                (display "Function name does not exist")
+                (display #\newline)
+                )
+              )
+          )
     )
+                      
                       
                       ;borat when clause. 'nawt nawt nawt'
                       ;THIS STATEMENT EXECUTES NOT
